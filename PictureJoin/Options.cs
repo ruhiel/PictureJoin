3ad718f288090e925c2e60bbe7bca385553bb9fd
ruhiel_murrue <ruhiel1983@gmail.com>
2017-11-28 23:32:10 +0900

    初回コミット

---
+++ PictureJoin/Options.cs
@@ -0,0 +1,57 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PictureJoin
+{
+    public class Options
+    {
+        //配列のオプション
+        [OptionArray('i', Required = true, HelpText ="入力ファイルパス")]
+        public string[] InputFiles { get; set; }
+
+        [Option('o', Required = true, HelpText = "出力ファイル")]
+        public string OutputFile { get; set; }
+
+        //(3)HelpOption属性
+        [HelpOption('h', "help")]
+        public string GetUsage()
+        {
+            var title = ((System.Reflection.AssemblyTitleAttribute)Attribute.GetCustomAttribute(
+                            System.Reflection.Assembly.GetExecutingAssembly(),
+                            typeof(System.Reflection.AssemblyTitleAttribute))).Title;
+            //ヘッダーの設定
+            var head = new HeadingInfo(
+                            title,
+                            System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString());
+
+            var asmcpy =
+                (System.Reflection.AssemblyCopyrightAttribute)
+                Attribute.GetCustomAttribute(
+                System.Reflection.Assembly.GetExecutingAssembly(),
+                typeof(System.Reflection.AssemblyCopyrightAttribute));
+
+            var regex = new Regex(@".+\s+(\d+)\s+(.+)");
+            var m = regex.Match(asmcpy.Copyright);
+
+            var help = new HelpText(head);
+            if (m.Success)
+            {
+                help.Copyright = new CopyrightInfo(m.Groups[2].Value, int.Parse(m.Groups[1].Value));
+            }
+
+            help.AddPreOptionsLine("オプション一覧");
+
+            //全オプションを表示(1行間隔)
+            help.AdditionalNewLineAfterOption = true;
+            help.AddOptions(this);
+
+            return help.ToString();
+        }
+    }
+}
--- PictureJoin/Program.cs
+++ PictureJoin/Program.cs
@@ -11,39 +11,39 @@ namespace PictureJoin
     {
         static void Main(string[] args)
         {
-            var bitmapList = new[]
+            var options = new Options();
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
             {
-                new Bitmap(@"D:\画像\アズールレーン\DPeoZeDVQAckY4f.jpg"),
-                new Bitmap(@"D:\画像\艦これ\DPeLW92UQAAsYZj.png")
-            };
-            
-            var records = bitmapList.Select(x => new {
-                Bitmap = x,
-                Height = bitmapList.Select(y => y.Height).Min(),
-                Width = x.Width * ((float)bitmapList.Select(y => y.Height).Min() / x.Height)
-            });
+                var bitmapList = options.InputFiles.Select(x => new Bitmap(x)).ToArray();
 
-            using (var img3 = new Bitmap(records.Sum(x => (int)x.Width), records.First().Height))
-            using (var g3 = Graphics.FromImage(img3))
-            {
-                var nowPointX = 0.0f;
+                var records = bitmapList.Select(x => new {
+                    Bitmap = x,
+                    Height = bitmapList.Select(y => y.Height).Min(),
+                    Width = x.Width * ((float)bitmapList.Select(y => y.Height).Min() / x.Height)
+                });
 
-                foreach (var record in records)
+                using (var img3 = new Bitmap(records.Sum(x => (int)x.Width), records.First().Height))
+                using (var g3 = Graphics.FromImage(img3))
                 {
-                    using (var g = Graphics.FromImage(record.Bitmap))
+                    var nowPointX = 0.0f;
+
+                    foreach (var record in records)
                     {
-                        g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
-                        g3.DrawImage(record.Bitmap, nowPointX, 0, record.Width, record.Height);
-                        nowPointX += record.Width;
+                        using (var g = Graphics.FromImage(record.Bitmap))
+                        {
+                            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+                            g3.DrawImage(record.Bitmap, nowPointX, 0, record.Width, record.Height);
+                            nowPointX += record.Width;
+                        }
                     }
+
+                    img3.Save(options.OutputFile, System.Drawing.Imaging.ImageFormat.Png);
                 }
 
-                img3.Save(@"D:\画像\アズールレーン\result.png", System.Drawing.Imaging.ImageFormat.Png);
-            }
-
-            foreach (var bitmap in bitmapList)
-            {
-                bitmap.Dispose();
+                foreach (var bitmap in bitmapList)
+                {
+                    bitmap.Dispose();
+                }
             }
         }
     }
